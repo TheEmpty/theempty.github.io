2048fb2c5e6deb1bc111b8b3b55ee24b40c41219
Myles Best <github@empty.best>
2026-02-05 09:44:22 +0900

    init

---
+++ _data/metadata.js
@@ -0,0 +1,10 @@
+export default {
+	title: "最高の日記の",
+	url: "https://最高の.com/",
+	language: "jp",
+	description: "アメリカ人は京都に住んでいる。日本語を勉強してる。ここはつまらないでしょう。", 
+	author: {
+		name: "Myles Best",
+		url: "https://myles.empty.best/about-me/"
+	}
+}
---
+++ content/blog/placeholder.md
@@ -0,0 +1,7 @@
+---
+title: すぐ行く
+description: Placeholder
+date: 2026-02-05
+tags: todo
+---
+まだ何も書いていません。
